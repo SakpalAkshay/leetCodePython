6094aedae964a69fd32861c06e5ffa2da8084923
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-01 16:38:31 -0700

    pushed description and solution of problem

--- plusOne_LC66.py
+++ plusOne_LC66.py
@@ -1,1 +1,20 @@
+'''
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
+Increment the large integer by one and return the resulting array of digits
+'''
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        summation = 0
+        length = len(digits)
+        for i in digits:
+            summation += i * pow(10,length-1)
+            length = length - 1
+        summation +=1
+
+        newStr = str(summation)
+        l = []
+        for i in newStr:
+            l.append(int(i))
+        return l
