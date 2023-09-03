6526cdd354881861b84f4d291df509af9ce81835
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-02 19:46:25 -0700

    pushed solution

--- removeDulicatesFromSortedArray.py
+++ removeDulicatesFromSortedArray.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i)>1:
+                j = nums.count(i)
+                while j > 1:
+                    nums.remove(i)
+                    j-=1
+        return len(nums)
