88cc0ae4ec67a97a5de97420b05901dc16d286e9
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-06 17:46:20 -0700

    pushed solution for majority element question

--- majorityElement_LC169.py
+++ majorityElement_LC169.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dicts = {}
+        for i in set(nums):
+            dicts[i] = nums.count(i)
+        size = len(nums)//2
+        for index in dicts.keys():
+            if dicts[index]>size:
+                return index
