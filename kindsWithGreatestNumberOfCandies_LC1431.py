55950dbe8b0198ab8de75c1f3f3a18cac7ea1b22
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-08 11:35:31 -0700

    pushed solution for kindsWithGreatestNumberOfCandies_LC1431.py

--- kindsWithGreatestNumberOfCandies_LC1431.py
+++ kindsWithGreatestNumberOfCandies_LC1431.py
@@ -4,3 +4,16 @@ There are n kids with candies. You are given an integer array candies, where eac
 Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.'''
 
 
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandyValue = max(candies)
+
+        result = []
+
+        for i in candies:
+            if(i+extraCandies>=maxCandyValue):
+                result.append(True)
+            else:
+                result.append(False)
+        
+        return result
