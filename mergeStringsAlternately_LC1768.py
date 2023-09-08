8b772ca1fa791dcc29f9b1ab22fa7aff1265857a
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-07 22:34:14 -0700

    pushed solution for mergeStringsAlternately_LC1768

--- mergeStringsAlternately_LC1768.py
+++ mergeStringsAlternately_LC1768.py
@@ -1,1 +1,22 @@
+lass Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        lenfirst = len(word1)
+        lenSec = len(word2)
+        newStr = ""
+        i =0
+        j=0
+        while i < lenfirst and j < lenSec:
+            newStr+= word1[i]
+            newStr+= word2[j]
+            i +=1
+            j +=1
+        
+        while i < lenfirst:
+            newStr+=word1[i]
+            i+=1
 
+        while j< lenSec:
+            newStr+=word2[j]
+            j+=1
+        
+        return newStr
