520551d593bb230f5c03958bda56c63aee6c079d
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-10 17:25:59 -0700

    pushed solution for contains Duplicate LC 217

--- containsDuplicate_LC217.py
+++ containsDuplicate_LC217.py
@@ -1,1 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        orgLength = len(nums)
+        setLength = len(set(nums))
 
+        if(orgLength==setLength):
+            return False
+        
+        return True
