afbd00430b761213c2aa3fc0a3eeb62c547661a6
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-11 21:54:55 -0700

    pushed solution missingNumber_LC268.py

--- missingNumber_LC268.py
+++ missingNumber_LC268.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        letMax = len(nums) 
+        for i in range(letMax + 1):
+            if i not in nums:
+              return i
