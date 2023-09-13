2674ddfe72fbd32201295fb96c440fd1635b7acd
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-12 22:51:00 -0700

    pushed another solution

--- missingNumber_LC268.py
+++ missingNumber_LC268.py
@@ -4,3 +4,13 @@ class Solution:
         for i in range(letMax + 1):
             if i not in nums:
               return i
+
+    def missingNumber2(self, nums: List[int]) -> int:
+        inputSum = 0 
+        for i in nums:
+            inputSum+=i
+        outSum = 0
+        for i in range(len(nums)+1):
+            outSum+=i
+
+        return outSum - inputSum
