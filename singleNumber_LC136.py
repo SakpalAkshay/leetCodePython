cc7b66b19f004e301dd5511b082a397b4dfea726
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-13 22:16:35 -0700

    pushed solution

--- singleNumber_LC136.py
+++ singleNumber_LC136.py
@@ -1,1 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        dicts = {}
+
+        for i in set(nums):
+            dicts[i] = nums.count(i)
+
+        
+        for i in dicts.keys():
+
+            if dicts[i] == 1:
+                return i
 
