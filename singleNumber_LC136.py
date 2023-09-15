268f9690e69cadc6a68c0384cb3753bac1962edb
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-14 21:21:17 -0700

    pushed another solution with less runtime

--- singleNumber_LC136.py
+++ singleNumber_LC136.py
@@ -12,3 +12,22 @@ class Solution:
             if dicts[i] == 1:
                 return i
 
+    def singleNumber2(self, nums: List[int]) -> int:
+        
+        #implementing count functionality to save time
+
+        dicts ={}
+
+        for i in nums:
+
+            if i in dicts:
+                dicts[i] += 1
+            else:
+                dicts[i] = 1
+
+        
+        for key, value in dicts.items():
+            if value == 1:
+                return key
+
+
