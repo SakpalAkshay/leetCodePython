a3494238277ac0601cbe3379b31109006697828f
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-16 22:48:31 -0700

    Pushed two pointer solution

--- squaresOfSortedArray_LC977.py
+++ squaresOfSortedArray_LC977.py
@@ -5,3 +5,18 @@ class Solution:
             l.append(i*i)
 
         return sorted(l)
+
+    def sortedSquares2(self, nums: List[int]) -> List[int]:
+        low = 0 
+        high = len(nums) - 1
+        sortedSq = []
+        while low <= high:
+
+            if abs(nums[low]) > nums[high]:
+                sortedSq.append(nums[low]*nums[low])
+                low+=1
+            else:
+                sortedSq.append(nums[high]*nums[high])
+                high -=1
+        
+        return sortedSq[::-1]
