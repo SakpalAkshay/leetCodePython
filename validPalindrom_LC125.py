64b294c09f4bb26d3f189da96b5dc60bfaa04429
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-10-03 16:50:25 -0700

    Pushed Solution

--- validPalindrom_LC125.py
+++ validPalindrom_LC125.py
@@ -1,1 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''.join(letter for letter in s if letter.isalnum()).lower()
+        left, right = 0, len(newStr) - 1
 
+        while left <= right:
+            if newStr[left] != newStr[right]:
+                return False
+            left +=1
+            right-=1
+        return True
+
+
+        
