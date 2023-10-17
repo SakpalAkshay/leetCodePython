591aaaf9eaf731a9e6d4041dcd9dede2c8f09ea0
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-10-17 14:30:49 -0700

    Pushed a two pointer solution

--- moveZeros_LC283.py
+++ moveZeros_LC283.py
@@ -7,3 +7,17 @@ class Solution:
             if i == 0:
                 nums.remove(i)
                 nums.append(0)
+    def moveZeroes2(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+
+        for fast in range(len(nums)):
+
+            if nums[fast] !=0 and nums[slow]==0:
+                nums[fast] , nums[slow] = nums[slow], nums[fast]
+                #we swap
+            
+            if nums[slow]!=0:
+                slow +=1
