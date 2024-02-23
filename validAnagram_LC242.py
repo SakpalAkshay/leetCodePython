533f459e40b1a66840437144285ba932d4899b8d
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-02-22 19:51:23 -0800

    pushed solution

--- validAnagram_LC242.py
+++ validAnagram_LC242.py
@@ -1,1 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sdict = {}
+        tdict = {}
 
+        if len(s) == 0 and len(t)==0:
+            return True
+
+        if len(s) != len(t):
+            return False
+
+        for i in s:
+            if i not in sdict:
+                sdict[i] = 1
+            else:
+                sdict[i] += 1
+        for i in t:
+            if i not in tdict:
+                tdict[i] = 1
+            else:
+                tdict[i] += 1
+        
+        for i in t:
+            if sdict.get(i) != tdict.get(i):
+                return False
+        
+        return True
