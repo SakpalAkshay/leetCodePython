bd739e6d3bb516babce83ef590b1f39a9618a663
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-02-28 22:26:52 -0800

    pushed code for Roman To Integer

--- RomanToInteger_LC13.py
+++ RomanToInteger_LC13.py
@@ -1,1 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {'I':1,  'V':5, 'X':10, 'L':50,  'C':100,  'D':500, 'M':1000}
+        op = 0
+        for i in range(len(s)):
+            #checking for Index error
+            if i + 1 < len(s) and values[s[i]] < values[s[i+1]]:
+                op -= values[s[i]]
+            else:
+                op += values[s[i]]
+
+        return op 
+
 
