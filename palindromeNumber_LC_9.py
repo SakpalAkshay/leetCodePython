ada5c5119ed37e73d66a449ac614e0847ecf2faa
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-03-03 15:19:13 -0800

    Pushed another solution using Math

--- palindromeNumber_LC_9.py
+++ palindromeNumber_LC_9.py
@@ -4,3 +4,18 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
 
         return str(x)==str(x)[::-1]
+
+
+
+    def anotherSolution(self,x: int) -> bool:
+        if x<0:
+            return False
+
+        n = x
+        rev = 0
+        while n != 0:
+            left = n % 10
+            n = n // 10
+            rev = rev * 10 + left
+
+        return rev == x
