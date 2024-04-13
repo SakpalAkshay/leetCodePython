d8c731a813d6223d5d0cdc3907a541f53cd4863d
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-12 19:45:02 -0700

    pushed solution

--- reverse_String_LC344.py
+++ reverse_String_LC344.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def reverseString(self, s: List[str]) -> None: 
+        start= 0 
+        end = len(s) - 1
+        temp = 0
+        while start < end:
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start = start + 1
+            end = end - 1
+        return s
