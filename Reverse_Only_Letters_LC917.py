3e6fe300e9bdff1bd78eac49973a126b7c02c599
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-15 14:42:14 -0700

    pushes solution

--- Reverse_Only_Letters_LC917.py
+++ Reverse_Only_Letters_LC917.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        lst = list(s)
+        i = 0
+        j = len(lst) - 1
+        while i < j:
+            if lst[i].isalpha() and lst[j].isalpha():
+                lst[i], lst[j] = lst[j], lst[i]
+                i += 1
+                j -= 1
+            else:
+                if lst[i].isalpha()==False:
+                    i +=1
+                if lst[j].isalpha()==False:
+                    j-=1
+        
+        return "".join(lst)
