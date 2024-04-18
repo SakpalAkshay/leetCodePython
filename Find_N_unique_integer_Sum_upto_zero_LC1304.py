3ce5ba929d68d8dbf4c18a1b7242721bceaf904c
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-17 19:44:29 -0700

    pushed solution

--- Find_N_unique_integer_Sum_upto_zero_LC1304.py
+++ Find_N_unique_integer_Sum_upto_zero_LC1304.py
@@ -1,1 +1,12 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        m = n // 2
+        out = []
+        for i in range(1,m+1):
+            out.append(i)
+            out.append(-i)
+        
+        if n % 2==1:
+            out.append(0)
+        return out
 
