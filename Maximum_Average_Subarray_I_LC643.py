3bb54bb93df255161720047fa472b8f4c216b745
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-19 21:58:17 -0700

    Pushed Another Solution

--- Maximum_Average_Subarray_I_LC643.py
+++ Maximum_Average_Subarray_I_LC643.py
@@ -14,5 +14,24 @@ class Solution:
         maximum = max(sum,maximum)
        return maximum/k
 
+    
+    
+    def findMaxAverage1(self, nums: List[int], k: int) -> float:
+        cur_sum = 0
+        res = float('-inf')
+
+        l = r = 0
+
+        while r < len(nums):
+            cur_sum += nums[r]
+
+            if r - l + 1 == k:
+                res = max(res, cur_sum)
+                cur_sum -= nums[l]
+                l += 1
             
+            r += 1
+        
+        return res / k
+
 
