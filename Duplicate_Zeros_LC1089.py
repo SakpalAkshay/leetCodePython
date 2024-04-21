bd0ccdf2bbf881c3d4f88d9e5f82435cdadf83a5
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-20 22:44:59 -0700

    Pushed O(n) Solution for Duplicate Zeros

--- Duplicate_Zeros_LC1089.py
+++ Duplicate_Zeros_LC1089.py
@@ -12,3 +12,14 @@ class Solution:
                 arr.pop()
             i+=1
         return arr
+    def duplicateZeros1(self, arr: List[int]) -> None:
+            zeroes = arr.count(0)
+            n = len(arr)
+            for i in range(n-1, -1, -1):
+                if i + zeroes < n:
+                    print(arr[i + zeroes],arr[i])
+                    arr[i + zeroes] = arr[i]
+                if arr[i] == 0: 
+                    zeroes -= 1
+                    if i + zeroes < n:
+                        arr[i + zeroes] = 0
