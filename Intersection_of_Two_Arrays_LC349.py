d45ee08cd5e2e7566673d603ef87e59687d706f4
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-22 18:09:25 -0700

    Pushed O(n) solution w/o Sets, used Hashing

--- Intersection_of_Two_Arrays_LC349.py
+++ Intersection_of_Two_Arrays_LC349.py
@@ -4,3 +4,16 @@ class Solution:
         s2 = set(nums2)
 
         return list(s1.intersection(s2))
+
+    def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        exist = {}
+        result = []
+        for i in nums1:
+            if i not in exist:
+                exist[i] = 1
+        
+        for j in nums2:
+            if j in exist and exist[j]==1:
+                result.append(j)
+                exist[j]+=1
+        return result
