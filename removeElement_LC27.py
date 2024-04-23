2708ab94ed019bb7dd8dc402ac179fdd400206a7
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-22 19:38:04 -0700

    Pushed O(n) Solution for the Same

--- removeElement_LC27.py
+++ removeElement_LC27.py
@@ -3,3 +3,32 @@ class Solution:
         while val in nums:
             nums.remove(val)
         return len(nums)
+   
+    def removeElement2(self, nums: List[int], val: int) -> int:
+
+        #Handling Few Edge Cases 
+        
+        if len(nums)==0:
+            return 0
+        
+        if len(nums)==1 and nums[0]==val:
+            return 0
+
+        if len(nums)==1 and nums[0]!=val:
+            return 1
+
+        # After Edge Cases 
+        i =0
+        j = len(nums) - 1
+
+        while i<=j:
+            if nums[j]==val:
+                j-=1
+                continue
+            elif nums[i]==val:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+            else:
+                i+=1
+
+        return j + 1
