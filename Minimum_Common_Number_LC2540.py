8e63ab57190fdccc170f71eab7448981953dc9e8
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-23 16:24:14 -0700

    pushed solution

--- Minimum_Common_Number_LC2540.py
+++ Minimum_Common_Number_LC2540.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        ifExist = set(nums1)
+        result = []
+        for i in nums2:
+            if i in ifExist:
+                result.append(i)
+        
+        if len(result) == 0:
+            return -1
+        else:
+            return result[0]
