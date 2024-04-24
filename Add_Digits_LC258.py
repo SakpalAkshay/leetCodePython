fa288816559f4dbe68b0701c1fcf735a9cb5924a
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-04-24 12:32:45 -0700

    pushed ssolution

--- Add_Digits_LC258.py
+++ Add_Digits_LC258.py
@@ -1,1 +1,12 @@
 
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num ==0:
+            return 0
+
+        while num >=10:
+            n = num%10
+            num = num//10
+            num = num + n 
+        return num
+        
