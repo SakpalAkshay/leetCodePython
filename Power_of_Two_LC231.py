432bcd6d6975d02a2a00a444ba69fd62feb658c9
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-05-18 20:25:41 -0700

    pushed solution

--- Power_of_Two_LC231.py
+++ Power_of_Two_LC231.py
@@ -1,1 +1,18 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n % 2 !=0:
+            return False
+        elif n <=0:
+            return False
+        else:
+            num = n
+
+            count = 0
+            while n!=1:
+                n = n//2
+                count+=1
+            if num == pow(2,count):
+                return True
 
