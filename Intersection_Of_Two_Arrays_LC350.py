16c5616b75bf0b21c30377f6f5e78ae9e9dc81d1
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-05-21 20:59:00 -0700

    Update Intersection_Of_Two_Arrays_LC350.py

--- Intersection_Of_Two_Arrays_LC350.py
+++ Intersection_Of_Two_Arrays_LC350.py
@@ -1,1 +1,31 @@
 
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        exist = {}
+        result = []
+        if len(nums1) > len(nums2):
+            for i in nums2:
+                if i in exist:
+                    exist[i]+=1
+                else:
+                    exist[i]=1
+            
+            for i in nums1:
+                if i in exist and exist[i]!=0:
+                    exist[i]-=1
+                    result.append(i)
+            
+
+            
+        else:
+            for i in nums1:
+                if i in exist:
+                    exist[i]+=1
+                else:
+                    exist[i]=1
+
+            for i in nums2:
+                if i in exist and exist[i]!=0:
+                    exist[i]-=1
+                    result.append(i)
+        return result
