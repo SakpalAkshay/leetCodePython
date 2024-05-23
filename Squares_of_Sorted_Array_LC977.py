e8da32c0c145df225c1fb6b1685d29a06d89c019
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-05-22 23:24:02 -0700

    Solved using Two Pointer Approach

--- Squares_of_Sorted_Array_LC977.py
+++ Squares_of_Sorted_Array_LC977.py
@@ -3,4 +3,22 @@ class Solution:
         square_val = [x*x for x in nums]
         square_val = sorted(square_val)
         return square_val
-      
+    
+    def sortedSquares2(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        sortedArr = [0]* len(nums)
+        index = right
+        while left<=right:
+            leftSquare = nums[left]*nums[left]
+            rightSquare = nums[right]*nums[right]
+
+            if rightSquare >= leftSquare :
+                sortedArr[index] = rightSquare
+                right -= 1
+                index-=1
+            else:
+                sortedArr[index] = leftSquare
+                left +=1
+                index-=1
+        return sortedArr
