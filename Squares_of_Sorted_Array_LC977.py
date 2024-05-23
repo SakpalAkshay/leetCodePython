25448c5d08ebab1fbd6607ed98d271c1b2a090c3
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-05-22 23:23:32 -0700

    Using Inbuilt function to solve the problem

--- Squares_of_Sorted_Array_LC977.py
+++ Squares_of_Sorted_Array_LC977.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        square_val = [x*x for x in nums]
+        square_val = sorted(square_val)
+        return square_val
+      
