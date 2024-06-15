efa317cb235cdd79e1267063f53e1308da8664f3
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-14 20:55:43 -0700

    First Approach in Solving

--- Remove_Linked_List_Elements_LC203.py
+++ Remove_Linked_List_Elements_LC203.py
@@ -1,1 +1,16 @@
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        fast = head
+        slow = None
 
+        while fast:
+            if fast.val ==val:
+                if slow:
+                    slow.next = fast.next
+                else:
+                    head = fast.next
+                fast = fast.next
+            else:
+                slow = fast
+                fast = fast.next
+        return head
