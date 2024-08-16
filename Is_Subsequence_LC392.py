b2c21e9ba18e14ac1b70443fa37e018e3a458cdf
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-08-15 23:33:53 -0700

    pushed solution

--- Is_Subsequence_LC392.py
+++ Is_Subsequence_LC392.py
@@ -1,1 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
 
+        j = 0
+        i = 0
+
+        if s == "":
+            return True
+
+        while i < len(t):
+            if s[j] == t[i]:
+                j+=1
+                if j > len(s) -1:
+                    return True
+            i+=1
+        
+        return False
