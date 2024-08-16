4d48d20dd191db6909d304b9719d301666d0f72a
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-08-15 23:11:43 -0700

    pushed solution

--- Find_Closest_to_Zero_LC2239.py
+++ Find_Closest_to_Zero_LC2239.py
@@ -1,1 +1,10 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        mini = nums[0]
 
+        for i in range(1, len(nums)):
+            if abs(nums[i]) < abs(mini):
+                mini = nums[i]
+        
+        if mini < 0 and abs(mini) in nums:
+            return abs(mini)
