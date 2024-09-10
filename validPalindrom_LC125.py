d490e1a6fcd568a799ef96a347f4f89270492f2a
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-09-10 16:06:29 -0700

    Pushed Recursion Based solution

--- validPalindrom_LC125.py
+++ validPalindrom_LC125.py
@@ -11,4 +11,22 @@ class Solution:
         return True
 
 
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+       
+        def check(i,s):
+            if i >= len(s) // 2:
+                return True
+            
+            if s[i] != s[len(s) - i -1]:
+                return False
+
+            return check(i+1,s)
+        
+        s = s.lower()
+        s = ''.join(e for e in s if e.isalnum())
+        if len(s) < 1:
+            return True
+        return check(0, s)
+
         
